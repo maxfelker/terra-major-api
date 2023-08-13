ef674dbb9828bdd4cdbc8873080401477ff6bc94
Max Felker <official@maxfelker.com>
2023-08-13 19:03:13 -0400

    Updated removed resolution struct, added resolutionPerPatch to json output

--- pkg/terrains/models/TerrainChunk.go
+++ pkg/terrains/models/TerrainChunk.go
@@ -10,17 +10,14 @@ import (
 
 type Heightmap [][]float64
 
-type Resolution struct {
-	X int `json:"x" gorm:"type:int"`
-	Y int `json:"y" gorm:"type:int"`
-}
-
 type TerrainChunk struct {
 	ID                  string                `gorm:"type:uuid;primary_key;unique;" json:"id"`
+	SandboxId           string                `gorm:"type:uuid;not null" json:"sandboxId"`
 	Position            sandboxModels.Vector3 `gorm:"type:jsonb;not null" json:"position"`
 	Dimension           int                   `json:"dimension" gorm:"type:int;not null"`
 	Height              int                   `json:"height" gorm:"type:int;not null"`
-	DetailResolution    Resolution            `json:"detailResolution" gorm:"type:jsonb;not null"`
+	DetailResolution    int                   `json:"detailResolution" gorm:"type:int;not null"`
+	ResolutionPerPatch  int                   `json:"resolutionPerPatch" gorm:"type:int;not null"`
 	HeightmapResolution int                   `json:"heightmapResolution" gorm:"type:int;not null"`
 	AlphamapResolution  int                   `json:"alphamapResolution" gorm:"type:int;not null"`
 	Heightmap           string                `json:"heightmap" gorm:"type:jsonb;not null"`
