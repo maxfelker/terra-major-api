e08c36e9358ae1e82ba60ddfd9fdd25274cd1d70
MW Felker <mwadefelker@gmail.com>
2023-12-07 21:23:37 -0500

    Added radius search for GetChunksBySandboxId

--- pkg/terrains/handlers/GetChunksBySandboxId.go
+++ pkg/terrains/handlers/GetChunksBySandboxId.go
@@ -2,37 +2,61 @@ package handlers
 
 import (
 	"encoding/json"
-	"errors"
 	"net/http"
+	"strconv"
 
-	"github.com/gorilla/mux"
 	"github.com/mw-felker/terra-major-api/pkg/core"
+	sandboxesModels "github.com/mw-felker/terra-major-api/pkg/sandboxes/models"
 	terrainModels "github.com/mw-felker/terra-major-api/pkg/terrains/models"
 	utils "github.com/mw-felker/terra-major-api/pkg/utils"
-
-	"gorm.io/gorm"
 )
 
 func GetChunksBySandboxId(app *core.App) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		query := request.URL.Query()
 
-		vars := mux.Vars(request)
-		sandboxId := vars["sandboxId"]
+		xQuery, _ := strconv.ParseFloat(query.Get("x"), 64)
+		x := float32(xQuery)
+
+		yQuery, _ := strconv.ParseFloat(query.Get("y"), 64)
+		y := float32(yQuery)
+
+		zQuery, _ := strconv.ParseFloat(query.Get("z"), 64)
+		z := float32(zQuery)
+
+		var queryPosition = sandboxesModels.Vector3{
+			X: &x,
+			Y: &y,
+			Z: &z,
+		}
+
+		if queryPosition.X == nil || queryPosition.Y == nil || queryPosition.Z == nil {
+			http.Error(writer, "All position fields (x, y, z) are required", http.StatusBadRequest)
+			return
+		}
+
+		radius, err := strconv.ParseFloat(query.Get("radius"), 32)
+		if err != nil {
+			http.Error(writer, "Invalid radius", http.StatusBadRequest)
+			return
+		}
 
 		var chunks []terrainModels.TerrainChunk
-		result := app.DB.Find(&chunks, "sandbox_id = ?", sandboxId)
+		result := app.DB.Raw(`
+			SELECT * FROM terrain_chunks
+			WHERE 
+			POWER(CAST(position->>'x' AS FLOAT) - ?, 2) +
+			POWER(CAST(position->>'y' AS FLOAT) - ?, 2) +
+			POWER(CAST(position->>'z' AS FLOAT) - ?, 2) 
+			<= POWER(?, 2)
+		`, queryPosition.X, queryPosition.Y, queryPosition.Z, radius).Scan(&chunks)
 
 		if result.Error != nil {
-			if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-				http.Error(writer, "Chunks not found with that sandboxId", http.StatusNotFound)
-			} else {
-				http.Error(writer, result.Error.Error(), http.StatusInternalServerError)
-			}
+			http.Error(writer, result.Error.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		response, err := json.Marshal(chunks)
-
 		if err != nil {
 			utils.ReturnError(writer, err.Error(), http.StatusInternalServerError)
 			return
